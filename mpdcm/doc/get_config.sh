97b695239c9af299472328729d3dc90db40f1f7d
Eduardo Aponte <aeduardo@biomed.ee.ethz.ch>
2014-11-10 18:34:57 +0100

    Minor changes.

--- mpdcm/doc/get_config.sh
+++ mpdcm/doc/get_config.sh
@@ -9,5 +9,5 @@ set -e
 gcc -v |& tail -n 1
 cat /etc/issue.net
 nvcc --version | tail -n 1
-lshw -numeric -C display 2>1 | grep product
+lshw -numeric -C display 2>&1 | grep product
 matlab -nodisplay -r "fprintf(1, version); fprintf(1, '\n'); exit;" | tail -n 2| head -n 1
