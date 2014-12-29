6e7c16150fa1d7f9c1af35fd5a294ffe13139581
Eduardo Aponte <aeduardo@biomed.ee.ethz.ch>
2014-12-29 16:08:05 +0100

    Last commit forgot to add two files.
    The new files provide a centralized function to get the means of the
    hemodynamic parameters.

---
+++ mpdcm/test/test_mpdcm_fmri_hb_map.m
@@ -0,0 +1,31 @@
+function test_mpdcm_fmri_hb_map(fp)
+%% Test 
+%
+% fp -- Pointer to a file for the test output, defaults to 1
+%
+% aponteeduardo@gmail.com
+% copyright (C) 2014
+%
+
+if nargin < 1
+    fp = 1;
+end
+
+fname = mfilename();
+fname = regexprep(fname, 'test_', '');
+
+
+fprintf(fp, '================\n Test %s\n================\n', fname);
+
+
+% Test whether there is any clear bug
+try
+    fprintf(fp, '       Passed\n');
+catch err
+    fprintf(fp, '   Not passed at line %d\n', err.stack(end).line);
+    fprintf(fp, getReport(err, 'extended'));
+end
+
+
+end
+
