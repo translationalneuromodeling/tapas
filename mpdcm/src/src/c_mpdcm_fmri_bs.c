84e0ae882605fe2d7cdf3af467524228d3694397
Eduardo Aponte <aeduardo@biomed.ee.ethz.ch>
2015-04-16 11:20:12 +0200

    Now includes the wrapper to the BS integrator.

---
+++ mpdcm/src/src/c_mpdcm_fmri_bs.c
@@ -0,0 +1,30 @@
+/* aponteeduardo@gmail.com */
+/* copyright (C) 2014 */
+
+#include "c_mpdcm.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+
+    if (nlhs > 1)
+    {
+        mexErrMsgIdAndTxt("CUDA_DCM:cuda_check_theta:input",
+            "Only one output supported");
+    }
+
+    if (nrhs != 3)
+    {
+        mexErrMsgIdAndTxt("CUDA_DCM:cuda_check_theta:input",
+            "Only three arguments supported");        
+    }
+
+
+    if ( nlhs == 1 )
+    {
+        int nx, ny, nu, dp, nt, nb;
+ 
+        c_mpdcm_fmri_bs(plhs, prhs, prhs+1, prhs+2, 
+            &nx, &nu, &ny, &dp, &nt, &nb); 
+    }
+
+} 
