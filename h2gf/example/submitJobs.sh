1ff737d64d9b091748ac21a12d584741e5b53563
siglesias <iglesias@biomed.ee.ethz.ch>
2018-11-06 16:00:24 +0100

    timing adapted

--- h2gf/example/submitJobs.sh
+++ h2gf/example/submitJobs.sh
@@ -2,16 +2,16 @@
 #
 
 #run h2gf srl2
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 04:00 -W 71:55 -o o_h2gf_1000_eta$k_m$i_config$j -e e_h2gf_1000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,1000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 02:00 -W 71:55 -o o_h2gf_3000_eta$k_m$i_config$j -e e_h2gf_3000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,3000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 23:30 -W 71:55 -o o_h2gf_4000_eta$k_m$i_config$j -e e_h2gf_4000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,4000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 22:00 -W 71:55 -o o_h2gf_5000_eta$k_m$i_config$j -e e_h2gf_5000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,5000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 04:00 -W 3:55 -o o_h2gf_1000_eta$k_m$i_config$j -e e_h2gf_1000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,1000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 02:00 -W 7:55 -o o_h2gf_3000_eta$k_m$i_config$j -e e_h2gf_3000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,3000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 23:30 -W 7:55 -o o_h2gf_4000_eta$k_m$i_config$j -e e_h2gf_4000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,4000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 22:00 -W 7:55 -o o_h2gf_5000_eta$k_m$i_config$j -e e_h2gf_5000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2($i,5000,$k,$j)"; done; done; done;
 
 #run h2gf srl1
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 21:00 -W 71:55 -o o_srl1_h2gf_1000_eta$k_m$i_config$j -e e_srl1_h2gf_1000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,1000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 19:00 -W 71:55 -o o_srl1_h2gf_3000_eta$k_m$i_config$j -e e_srl1_h2gf_3000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,3000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 17:00 -W 71:55 -o o_srl1_h2gf_4000_eta$k_m$i_config$j -e e_srl1_h2gf_4000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,4000,$k,$j)"; done; done; done;
-for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -W 71:55 -o o_srl1_h2gf_5000_eta$k_m$i_config$j -e e_srl1_h2gf_5000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,5000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 21:00 -W 3:55 -o o_srl1_h2gf_1000_eta$k_m$i_config$j -e e_srl1_h2gf_1000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,1000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 19:00 -W 7:55 -o o_srl1_h2gf_3000_eta$k_m$i_config$j -e e_srl1_h2gf_3000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,3000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -b 17:00 -W 7:55 -o o_srl1_h2gf_4000_eta$k_m$i_config$j -e e_srl1_h2gf_4000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,4000,$k,$j)"; done; done; done;
+for i in {1..12}; do for k in {1..6}; do for j in {1..12};do bsub -W 7:55 -o o_srl1_h2gf_5000_eta$k_m$i_config$j -e e_srl1_h2gf_5000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl1($i,5000,$k,$j)"; done; done; done;
 
 #run h2gf_rw srl2
 for i in {1..12}; do for k in {1..6}; do bsub -b 09:00 -W 3:55 -o o_h2gf_1000_eta$k_m$i_config$j -e e_h2gf_1000_eta$k_m$i_config$j matlab -singleCompThread -r "h2gf_demo_srl2_rw($i,1000,$k)"; done; done; 
