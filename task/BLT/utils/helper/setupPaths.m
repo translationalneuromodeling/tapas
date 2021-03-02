cb7c7c5101ce8213ed00ea7ea34db0c673ae8165
[ofaull] <[faull@biomed.ee.ethz.ch]>
2021-03-02 16:33:45 +1300

    Added and removed necessary files and folders

--- task/BLT/utils/helper/setupPaths.m
+++
@@ -1,6 +0,0 @@
-pathCogent='C:\fMRI_paradigmas\JHeinzle\Cogent2000v1.32\Toolbox';
-pathPsychtoolbox='C:\fMRI_paradigmas\Psychtoolbox\Psychtoolbox';
-addpath(pathCogent);
-addpath(genpath(pathPsychtoolbox));
-rmpath(fullfile(pathPsychtoolbox,'PsychBasic','MatlabWindowsFilesR2007a')); % To get order of path right for PTB
-addpath(fullfile(pathPsychtoolbox,'PsychBasic','MatlabWindowsFilesR2007a'));
