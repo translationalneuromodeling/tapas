97560a0f510b84f8449e5fea923245c0af3ad7f8
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-08-09 19:40:26 +1000

    function place holder

---
+++ PhysIO/code/readin/tapas_physio_write2bids.m
@@ -0,0 +1,10 @@
+function [t,c,r,svolpulse]= tapa_physio_write2bids(physio, varargin)
+% Converts trigger cardiac and respiratory data from physio structure into
+% a tsv file according to BIDS format, with meta data and tirgger data
+% in json file
+
+
+
+
+
+end
