aafc907d75ff8fe8a703b43b42830abf32873358
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-08-09 19:45:44 +1000

    update

--- PhysIO/code/readin/tapas_physio_write2bids.m
+++ PhysIO/code/readin/tapas_physio_write2bids.m
@@ -1,7 +1,14 @@
-function [t,c,r,svolpulse]= tapa_physio_write2bids(physio, varargin)
+function [t,c,r,svolpulse]= tapa_physio_write2bids(physio)
 % Converts trigger cardiac and respiratory data from physio structure into
-% a tsv file according to BIDS format, with meta data and tirgger data
+% a tsv file according to BIDS format, with meta data 
 % in json file
+
+% IN: physio structure
+%     
+
+% OUT: tsv file(s) with columns caridac, respiratory, trigger
+%    json file with meta data
+
 
 
 
