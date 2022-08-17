098de0be1ff8438387784fbdf7fcc394c1bc01a6
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-08-17 20:44:42 +1000

    add documentation

--- PhysIO/code/readin/tapas_physio_write2bids.m
+++ PhysIO/code/readin/tapas_physio_write2bids.m
@@ -11,10 +11,14 @@ function [t,c,r,svolpulse]= tapa_physio_write2bids(physio)
 
 cardiac = physio.ons_secs.c;
 respiratory = physio.ons_secs.r;
+%TODO write to file
 
+% TODO this needs to be updated to change by subjects - can this info be
+% taken from the pysio structure?
 subj='subject1'
 session= 'session1'
 
+% prepare structure to write into BIDS
  s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
         "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
    
@@ -22,6 +26,7 @@ session= 'session1'
 JSONFILE_name= sprintf('%s_JSON%d.json',subj, session); 
     fid=fopen(JSONFILE_name,'w') 
     encodedJSON = jsonencode(s); 
+    % TODO: think about and add output folder
     fprintf(fid, encodedJSON); 
 
 end
