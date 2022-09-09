a59ff0952482b75db5357b4bb42762110c5930e0
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-09-09 19:40:17 +1000

    change write bids input

--- PhysIO/code/tapas_physio_main_create_regressors.m
+++ PhysIO/code/tapas_physio_main_create_regressors.m
@@ -90,7 +90,7 @@ else % assemble physio-structure
     physio.model        = varargin{4};
     physio.verbose      = varargin{5};
     physio.save_dir     = varargin{6};
-    physio.writebids    = varargin{7};
+    physio.write_bids    = varargin{7};
 end
 
 % fill up empty parameters
