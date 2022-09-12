ab574f1f38cd969584c247ce75a478563d89f98c
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-09-12 15:36:33 +1000

    add physio structure as input

--- PhysIO/code/readin/tapas_physio_write2bids.m
+++ PhysIO/code/readin/tapas_physio_write2bids.m
@@ -1,4 +1,4 @@
-function []= tapa_physio_write2bids(ons_secs, location)
+function []= tapa_physio_write2bids(ons_secs, physio, location)
 % Converts trigger, cardiac and respiratory data from physio structure into
 % a .tsv file according to BIDS format, with meta data in a corresponding json file.
 
@@ -22,7 +22,7 @@ save_dir=physio.write_bids
 % after step1
 switch location
     case 1 
-        tag = ""
+        tag = "loc1"
         cardiac = ons_secs.c;
         respiratory = ons_secs.r;
 
@@ -35,53 +35,60 @@ switch location
         encodedJSON = jsonencode(s); 
         % write output
         fprintf(fid, encodedJSON); 
-
+        writematrix(cardiac,fullfile(save_dir,'cardiac.txt'),'Delimiter','tab')
+        writematrix(respiratory,fullfile(save_dir,'respiratory.txt'),'Delimiter','tab')
+       
+ 
     case 2
-    tag = ""
-    cardiac = ons_secs.c;
-    respiratory = ons_secs.r;
+        tag = "normalized"
+        cardiac = ons_secs.c;
+        respiratory = ons_secs.r;
 
-    s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
-    "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
+        s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
+        "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
 
-     % create JSON file
-     JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
-     fid = fopen(fullfile(save_dir,JSONFILE_name),'w'); 
-     encodedJSON = jsonencode(s); 
-     % write output
-     fprintf(fid, encodedJSON);
+        % create JSON file
+        JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
+        fid = fopen(fullfile(save_dir,JSONFILE_name),'w'); 
+        encodedJSON = jsonencode(s); 
+        % write output
+        fprintf(fid, encodedJSON);
+
+        writematrix(cardiac,fullfile(save_dir,'cardiac.txt'),'Delimiter','tab')
+        writematrix(respiratory,fullfile(save_dir,'respiratory.txt'),'Delimiter','tab')
+   
 
     case 3
-    tag = "trigger"
-    % triggerafter step 2
-    cardiac = ons_secs.c;
-    respiratory = ons_secs.r;
+        tag = "trigger"
+        % triggerafter step 2
+        cardiac = physio.ons_secs.c;
+        respiratory = physio.ons_secs.r;
+    
+        trigger= physio.ons_secs.svolpulse;
+    
+        % triggers have to be replaced into 1 (trigger) 0 (no trigger)
+    
+        trigger_binary=zeros(numel(physio.ons_secs.t),1);
 
-    trigger= physio.ons_secs.svolpulse;
-
-    % triggers have to be replaced into 1 (trigger) 0 (no trigger)
-
-    trigger_binary=zeros(numel(physio.ons_secs.t),1);
-
-    for iVolume = 1:numel(physio.ons_secs.svolpulse)
-        row_number = physio.ons_secs.svolpulse(iVolume)==physio.ons_secs.t;
-        trigger_binary(row_number)=1;
-    end
-
-    % prepare structure to write into BIDS
-    s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
-        "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory", "trigger"]); 
-
-    % create JSON file
-    JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
-    fid=fopen(fullfile(save_dir,JSONFILE_name),'w'); 
-    encodedJSON = jsonencode(s); 
-   % write output
-    fprintf(fid, encodedJSON); 
-
-    % write output
-    writematrix(cardiac,fullfile(save_dir,'cardiac.txt'),'Delimiter','tab')
-    writematrix(respiratory,fullfile(save_dir,'respiratory.txt'),'Delimiter','tab')
-    writematrix(trigger_binary,fullfile(save_dir,'trigger_binary.txt'),'Delimiter','tab')
+        for iVolume = 1:numel(physio.ons_secs.svolpulse)
+            row_number = physio.ons_secs.svolpulse(iVolume)==physio.ons_secs.t;
+            trigger_binary(row_number)=1;
+        end
+        
+        % prepare structure to write into BIDS
+        s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
+            "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory", "trigger"]); 
+        
+        % create JSON file
+        JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
+        fid=fopen(fullfile(save_dir,JSONFILE_name),'w'); 
+        encodedJSON = jsonencode(s); 
+        % write output
+        fprintf(fid, encodedJSON); 
+        
+        % write output
+        writematrix(cardiac,fullfile(save_dir,'cardiac.txt'),'Delimiter','tab')
+        writematrix(respiratory,fullfile(save_dir,'respiratory.txt'),'Delimiter','tab')
+        writematrix(trigger_binary,fullfile(save_dir,'trigger_binary.txt'),'Delimiter','tab')
 
 end
