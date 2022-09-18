4494184cbf81e63d489435b2d072dcd5805f71ff
Johanna Bayer <johanna.bayer@orygen.org.au>
2022-09-18 16:39:11 +1000

    make changes to input

--- PhysIO/code/readin/tapas_physio_write2bids.m
+++ PhysIO/code/readin/tapas_physio_write2bids.m
@@ -1,11 +1,12 @@
-function []= tapa_physio_write2bids(ons_secs, physio)
+function []= tapa_physio_write2bids(ons_secs, write_bids, save_dir, log_files)
 % Converts trigger, cardiac and respiratory data from physio structure into
 % a .tsv file according to BIDS format, with meta data in a corresponding json file.
 
 
-% IN: ons_secs structure
-%     location parameter describing the stage of the processing pipeline
-%              at which the files are being written
+% IN: ons_secs:     onsecs structure
+%     write_bids:   parameter describing the stage of the processing pipeline
+%                   at which the files are being written
+%    save_dir: where the file should be written to
         
 
 % OUT: tsv file(s) caridac, respiratory, trigger
@@ -17,18 +18,17 @@ function []= tapa_physio_write2bids(ons_secs, physio)
 
 % Author: Johanna Bayer 2022
 
-save_bids_dir=physio.write_bids.dir
-location = physio.write_bids.location
+
 
 % after step1
-switch location
+switch write_bids
     case 1 
         tag = "loc1"
         cardiac = ons_secs.c;
         respiratory = ons_secs.r;
 
-        s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
-        "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
+        s = struct("StartTime", log_files.relative_start_acquisition , ...
+        "SamplingFrequency",log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
 
         % create JSON file
         JSONFILE_name= sprintf('%s_JSON.json',tag); 
@@ -45,8 +45,8 @@ switch location
         cardiac = ons_secs.c;
         respiratory = ons_secs.r;
 
-        s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
-        "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
+        s = struct("StartTime", log_files.relative_start_acquisition , ...
+        "SamplingFrequency",log_files.sampling_interval, "Columns", ["cardiac", "respiratory"]); 
 
         % create JSON file
         JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
@@ -62,23 +62,23 @@ switch location
     case 3
         tag = "trigger"
         % triggerafter step 2
-        cardiac = physio.ons_secs.c;
-        respiratory = physio.ons_secs.r;
+        cardiac = ons_secs.c;
+        respiratory = ons_secs.r;
     
-        trigger= physio.ons_secs.svolpulse;
+        trigger= ons_secs.svolpulse;
     
         % triggers have to be replaced into 1 (trigger) 0 (no trigger)
     
-        trigger_binary=zeros(numel(physio.ons_secs.t),1);
+        trigger_binary=zeros(numel(ons_secs.t),1);
 
-        for iVolume = 1:numel(physio.ons_secs.svolpulse)
-            row_number = physio.ons_secs.svolpulse(iVolume)==physio.ons_secs.t;
+        for iVolume = 1:numel(ons_secs.svolpulse)
+            row_number = ons_secs.svolpulse(iVolume)==ons_secs.t;
             trigger_binary(row_number)=1;
         end
         
         % prepare structure to write into BIDS
-        s = struct("StartTime", physio.log_files.relative_start_acquisition , ...
-            "SamplingFrequency",physio.log_files.sampling_interval, "Columns", ["cardiac", "respiratory", "trigger"]); 
+        s = struct("StartTime", log_files.relative_start_acquisition , ...
+            "SamplingFrequency",log_files.sampling_interval, "Columns", ["cardiac", "respiratory", "trigger"]); 
         
         % create JSON file
         JSONFILE_name= sprintf('%s_%s_JSON.json',tag); 
