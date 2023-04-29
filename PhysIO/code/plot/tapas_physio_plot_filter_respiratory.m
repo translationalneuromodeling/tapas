2a091796accefbdb5491955b356ae9aff6e9114a
Johanna Bayer <johanna.bayer@orygen.org.au>
2023-04-29 14:13:32 +1000

    add functions to plotting func

--- PhysIO/code/plot/tapas_physio_plot_filter_respiratory.m
+++ PhysIO/code/plot/tapas_physio_plot_filter_respiratory.m
@@ -1,13 +1,33 @@
-function [verbose, t] = tapas_physio_plot_filter_respiratory(rpulset,rsampint, verbose)
+function [verbose, handles, labels] = tapas_physio_plot_filter_respiratory(rpulset,m, s, t, ...
+    rpulset_out, rpulset_out_trend, trend, rpulset_out_trend_filt, verbose)
 % plot repiratory filtering 
 % (C) 2023 Johanna Bayer
     verbose.fig_handles(end+1) = tapas_physio_get_default_fig_params();
     set(gcf, 'Name', 'Preproc: Respiratory filtering');
     hold on;
     handles = []; labels = {};
-    t = linspace(0.0, rsampint * (length(rpulset) - 1), length(rpulset));
     plot([t(1), t(end)], [0.0, 0.0], 'Color', [0.7, 0.7, 0.7]);
-    m = mean(rpulset); s = std(rpulset);
     handles(end+1) = plot(t, (rpulset - m) / s);
     labels{end+1} = 'Raw respiratory signal';
+
+    % without outliers
+    handles(end+1) = plot(t, (rpulset_out - m) / s);
+    labels{end+1} = '... without outliers';
+
+    % detrend
+    figure(verbose.fig_handles(end));
+    handles(end+1) = plot(t, (trend - m) / s);
+    labels{end+1} = '... low frequency trend';
+    plot([t(1), t(end)], [-5.0, -5.0], 'Color', [0.7, 0.7, 0.7]);
+    handles(end+1) = plot(t, (rpulset_out_trend - m) / s - 5.0);
+    labels{end+1} = '... detrended';
+
+    % Low pass filtered to remove noise
+    handles(end+1) = plot(t, (rpulset_out_trend_filt - m) / s - 5.0);
+    labels{end+1} = '... after low-pass filter';
+    xlim([t(1), t(end)]);
+    xlabel('Time (s)');
+    yticks([]);
+    legend(handles, labels);
+
 end
