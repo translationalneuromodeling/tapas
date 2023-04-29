31f90e3a0b813c0660ede4e4a1abf590ca83dfc0
Johanna Bayer <johanna.bayer@orygen.org.au>
2023-04-29 16:49:32 +1000

    isolate plot function

--- PhysIO/code/preproc/tapas_physio_get_cardiac_pulse_template.m
+++ PhysIO/code/preproc/tapas_physio_get_cardiac_pulse_template.m
@@ -52,76 +52,40 @@ dt = t(2) - t(1);
 hasFirstGuessPeaks = ~isempty(cpulse1stGuess);
 
 
-%% Second step, refined heart rate estimate
-
-stringTitle = 'Preproc: Iterative Template Creation Single Cycle';
-     
 if hasFirstGuessPeaks
-    
+
     averageHeartRateInSamples = round(mean(diff(cpulse1stGuess)));
     [tmp, cpulse2ndGuess] = tapas_physio_findpeaks(c,...
         'minpeakheight', thresh_min,...
         'minpeakdistance', round(shortenTemplateFactor*...
         averageHeartRateInSamples));
-    
+
     if doDebug
-        
+
+%% Second step, refined heart rate estimate
+
         nPulses1 = length(cpulse1stGuess);
         nPulses2 = length(cpulse2ndGuess);
-        fh = tapas_physio_get_default_fig_params();
-        set(fh, 'Name', stringTitle);
-        verbose.fig_handles(end+1) = fh;
-        subplot(3,1,1);
-        hold off
-        hp(3) = plot(t, c, 'k');
-        hold all;
-        hp(1) = stem(t(cpulse1stGuess), ...
-            4*ones(nPulses1,1),'b');
-        
-        hp(2) = stem(t(cpulse2ndGuess),...
-            4*ones(nPulses2,1),'r');
-     
-        stringLegend = {
-            sprintf('1st guess peaks (N =%d)', nPulses1), ...
-            sprintf('2nd guess peaks (N =%d)', nPulses2), ...
-            'raw time series'
-            };
-    
-        legend(hp, stringLegend);
-        title('Finding first peak (cycle start), backwards')
-        
-        
-        
-        %% Plot difference between detected events
-        subplot(3,1,2);
-        
+
         meanLag1 = mean(diff(t(cpulse1stGuess)));
         meanLag2 = mean(diff(t(cpulse2ndGuess)));
-        
-        plot(t(cpulse1stGuess(2:end)), diff(t(cpulse1stGuess)));
-        hold all
-        plot(t(cpulse2ndGuess(2:end)), diff(t(cpulse2ndGuess)));
-        title('Temporal lag between events')
-        
-         stringLegend = {
-            sprintf('1st Guess (Mean lag duration %3.1f s)', meanLag1), ...
-            sprintf('2nd Guess (Mean lag duration %3.1f s)', meanLag2) ...
-            };
-        
-        legend(stringLegend);
+
     end
-else
-    if doDebug
-        fh = tapas_physio_get_default_fig_params();
-        verbose.fig_handles(end+1) = fh;
-        subplot(3,1,1);
-        plot(t, c, 'k'); title('Preproc: Finding first peak of cycle, backwards')
-    end
+
+else 
     verbose = tapas_physio_log(['No peaks found in raw cardiac time series. Check raw ' ...
         'physiological recordings figure whether there is any non-constant ' ...
         'cardiac data'], verbose, 2); % error!
-end
+end 
 
+%% Plot in case of Debugging (verbose =>3)
+
+if doDebug
+ 
+    [verbose] = tapas_physio_plot_iterative_template_creation(hasFirstGuessPeaks,...
+    t, c, cpulse1stGuess, nPulses1, nPulses2, cpulse2ndGuess, meanLag1, meanLag2, verbose)
+
+end 
 
 
 %% Build template based on the guessed peaks:
