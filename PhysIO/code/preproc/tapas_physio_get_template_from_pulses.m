eff9f7614458be4bd4918fa5325e1095b450ac9d
Johanna Bayer <johanna.bayer@orygen.org.au>
2023-04-30 12:37:28 +1000

    remove plot function

--- PhysIO/code/preproc/tapas_physio_get_template_from_pulses.m
+++ PhysIO/code/preproc/tapas_physio_get_template_from_pulses.m
@@ -94,19 +94,8 @@ template(1,:) = [];
 
 % template as average of the found representative waves
 pulseTemplate = mean(template);
+tTemplate = dt*(0:2*halfTemplateWidthInSamples);
 
-if doDebug
-    fh = verbose.fig_handles(end);
-    figure(fh);
-    subplot(3,1,3);
-    tTemplate = dt*(0:2*halfTemplateWidthInSamples);
-    plot(tTemplate, template');
-    hold all;
-    hp(1) = plot(tTemplate, pulseTemplate', '.--g', 'LineWidth', 3, 'Marker', ...
-        'o');
-    xlabel('t (seconds)');
-    title('Templates of cycle time course and mean template');
-end
 
 %% Final template for peak search from best-matching templates
 % delete the peaks deviating from the mean too
@@ -140,9 +129,9 @@ end
 pulseCleanedTemplate = mean(template(indHighQualityTemplates, :));
 
 if doDebug
-    stringTitle = 'Preproc: Iterative Template Creation Single Cycle';
-    hp(2) = plot(tTemplate, pulseCleanedTemplate, '.-g', 'LineWidth', 4, ...
-        'Marker', 'x');
-    legend(hp, 'mean of templates', 'mean of most similar, chosen templates');
-    tapas_physio_suptitle(stringTitle);
+ 
+    [verbose] = tapas_physio_plot_templates_of_cycle_time(tTemplate, ...
+    template, pulseTemplate, pulseCleanedTemplate, verbose);
+
+    
 end
