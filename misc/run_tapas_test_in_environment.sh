db15018953bff463be05f8698b685db7578158f8
Matthias Mueller-Schrader <muellmat@ethz.ch>
2023-05-08 23:23:59 +0200

    Add script for testing-environment.

---
+++ misc/run_tapas_test_in_environment.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# 
+# RUN_TAPAS_TEST_IN_ENVIRONMENT
+#
+# Clone/download tapas,spm and tapas-examples to temporal folder and 
+# run testing pipeline in 'isolated environment' (clean matlab).
+#
+# Authors: Matthias Müller-Schrader & Lars Kasper
+# Created: 2023-05-08
+# Copyright (C) 2023 TNU, Institute for Biomedical Engineering,
+#                    University of Zurich and ETH Zurich.
+#
+# This file is part of the TAPAS PhysIO Toolbox, which is released under
+# the terms of the GNU General Public License (GPL), version 3. You can
+# redistribute it and/or modify it under the terms of the GPL (either
+# version 3 or, at your option, any later version). For further details,
+# see the file COPYING or <http://www.gnu.org/licenses/>.
+#
+
+## Parameters
+# TODO: Use getopts.
+TMPDIR=`mktemp -d -t tapas_test.XXX` # TODO: Check if that will work under mac. 
+TAPASREPO="git@tnurepository.ethz.ch:TNU/tapas.git"
+BRANCH="tapas-v-6-1-0"
+TOOLBOX="physio"
+DESKTOPOPT='-nodesktop'
+
+## Create directory
+echo "Using temporal directory $TMPDIR"
+touch "${TMPDIR}/tapas-test.log"
+echo "MATLAB-log will be written to ${TMPDIR}/tapas-test.log"
+## Copy script in there
+cp tapas_test_in_environment_template.m "${TMPDIR}/tapas_test_in_environment.m"
+cd "$TMPDIR"
+
+## Clone tapas and spm
+git clone --depth=1 --branch "$BRANCH" --single-branch "$TAPASREPO"
+git clone --depth=1 https://github.com/spm/spm.git # Development version
+
+## Start matlab:
+echo "Everything is ready - starting matlab"
+matlab -sd "$TMPDIR" $DESKTOPOPT -r "tapas_test_in_environment ${TOOLBOX} "
+
+# We don't delete temporal directory, since a) that can be often done by UNIX 
+# and b) allows us to inspect the directory for debugging.
+exit
---
+++ misc/tapas_test_in_environment_template.m
@@ -0,0 +1,41 @@
+function worked = tapas_test_in_environment(toolbox)
+% TAPAS_TEST_IN_ENVIRONMENT run test in separate environment
+%
+% IN 
+%    toolbox [character]
+%	Toolbox to test. If empty, all toolboxes will be tested.
+% 
+% Authors: Matthias Müller-Schrader & Lars Kasper
+% Created: 2023-05-08
+% Copyright (C) 2023 TNU, Institute for Biomedical Engineering,
+%                    University of Zurich and ETH Zurich.
+%
+% This file is part of the TAPAS PhysIO Toolbox, which is released under
+% the terms of the GNU General Public License (GPL), version 3. You can
+% redistribute it and/or modify it under the terms of the GPL (either
+% version 3 or, at your option, any later version). For further details,
+% see the file COPYING or <http://www.gnu.org/licenses/>.
+%
+
+% NOTE: This file has a different filename than the function it defines.
+% The reason is that it will be copied by run_tapas_test_in_environment 
+% to a file with the proper name (to avoid shadowing of the path).
+
+diary('tapas-test.log')
+if nargin < 1 || isempty(toolbox)
+   disp('Testing all tooboxes!')
+    toolbox = {}; % Everything.
+elseif ~iscell(toolbox)
+    toolbox = {toolbox};
+end
+
+fprintf('Running tapas_test_in_environment at %s\n',datetime)
+restoredefaultpath; % Ensure that path is clean.
+addpath(fullfile(pwd,'spm')); % Add SPM-folder.
+cd('tapas'); % Go into tapas-folder to init tapas
+tapas_init(toolbox{:}); % Init tapas.
+cd('..') % Execute rest in tapas-folder.
+tapas_download_example_data(); % Download example data for the tests.
+nFailed = tapas_test(toolbox{:}); % Run the tests.
+diary('off')
+exit(min(nFailed,255)); % Exit > 0: We had a problem.
